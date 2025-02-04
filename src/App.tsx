0181d394549843281308d1b736689190f1dc7d01
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2025-02-03 23:14:48 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import "./scss/App.scss";
+import { useState, useEffect } from 'react';
+
+//-06:00 for CST
+const startDate = new Date("2025-02-04T00:00:00-06:00");
+
+function getUpdatedDate() {
+	const now = new Date();
+	const differenceInDates = now.getTime() - startDate.getTime();
+	const newDate = new Date(startDate.getTime() - differenceInDates);
+
+	return newDate;
+}
+
+function App() {
+	const [displayedDate, setDisplayedDate] = useState<Date>(getUpdatedDate());
+
+	useEffect(() => {
+		let interval = setInterval(() => {
+			setDisplayedDate(getUpdatedDate());
+		}, 250);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<>
+			<header>
+				<h1 className="site-name">
+					Rewinder
+				</h1>
+				<p className="site-info">
+					Rewinder shows time going in reverse from <span>February 4th, 2025 12am CST</span>
+				</p>
+			</header>
+
+			<main>
+				<p className="displayed-date">
+					{ displayedDate.toLocaleString() }
+				</p>
+			</main>
+		</>
+	);
+}
+
+export default App;
